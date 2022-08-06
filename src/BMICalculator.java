6724e1e7183bd368ba0d4e65718bacc89a34df84
Hara <harachung90@gmail.com>
2022-08-06 19:29:10 +1000

    Second commit. Calculator done. When the height (in cm) and weight (in kg) are input, the program calculates the BMI.

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -1,19 +1,37 @@
-
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BMICalculator {
+public class BMICalculator extends JFrame {
 
     private JTextField heightText;
-    private JTextField Weight;
+    private JTextField weightText;
     private JButton Calculate;
     private JLabel heightLabel;
     private JLabel weightLabel;
-    private JLabel ResultLabel;
+    private JLabel resultLabel;
+    private JPanel mainPanel;
+
+    public BMICalculator(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+
+        Calculate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double heightInput = (Double.parseDouble((heightText.getText())) / 100);
+                double weightInput = (Double.parseDouble((weightText.getText())));
+
+                double result = weightInput / (heightInput * heightInput);
+                resultLabel.setText("Your BMI is: " + String.format("%.2f", result));
+            }
+        });
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("BMI Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        JFrame frame = new BMICalculator("BMI Calculator");
         frame.setVisible(true);
     }
 }
